ac35bcd7ef17e1cfacb1523f4b727142ecfa7c9e
IamGOOD <e.rukshenas@gmail.com>
2025-02-23 00:34:27 -0500

    Class Views, Forms

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .form import EmailPostForm
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    template_name = 'blog/post/list.html'
+    paginate_by = 3
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            # ... send email
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
+    )
+
+
+'''
 def post_list(request):
     post_list = Post.published.all()
     # Pagination with 3 posts per page
@@ -19,6 +54,8 @@ def post_list(request):
         'blog/post/list.html',
         {'posts': posts}
     )
+
+'''
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
